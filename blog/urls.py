2d1590b928991624a3ebe8948cf579314c0bc533
st129san <musashi.takano1222@gmail.com>
2025-06-22 10:16:05 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     #path arg1:designate letters pattern on URL 2:the view to be called when URL pattern is matched 3:pattern's name
     path('', views.post_list, name='post_list'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
